623eb9cb9ab47981a9f0524b36ec3a57481e69e5
Avinash Bhaskar <avinash.bhaskar@sap.com>
2019-07-01 16:31:42 +0530

    Completed the module

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -5,9 +5,8 @@ class QuizQuestion extends Component {
     return (
       <main>
         <section>
-          <p> {this.props.quiz_question.instruction_text} </p>
+          <p>{this.props.quiz_question.instruction_text}</p>
         </section>
-
         <section className="buttons">
           <ul>
             <li> {this.props.quiz_question.answer_options[0]}</li>
